4086afa6315aa1fc0c95a044864d7849f4caf5c1
Gilvan Ribeiro De Almeida <g.r.almeida@live.com>
2022-09-23 10:13:54 -0300

    Iniciando o projeto

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/grbalmeida/curso-go-desenvolvendo-api-rest-alura
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Home Page")
+}
+
+func HandleRequest() {
+	http.HandleFunc("/", Home)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func main() {
+	HandleRequest()
+}
